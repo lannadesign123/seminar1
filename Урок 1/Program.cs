4618e4fe1c887fd42236be075af6b3103332a8d3
svetlana <lannadesign123@gmail.com>
2022-11-14 05:32:17 +0300

    урок1

---
+++ Урок 1/Program.cs
@@ -0,0 +1,79 @@
+﻿/*Console.Write("Good morning!");
+
+int number = 11;
+
+Console.WriteLine("Good morning! Today " + number + " november");
+
+Console.Write($"Good morning! Today {number}  november");
+*/
+
+//Console.WriteLine("input your number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+// Напишите прграмму, которая на вход принимает число и выдает его квадрат
+
+/*Console.WriteLine("Введите число, которого нужно найти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number * number;
+
+//Console.WriteLine($"Квадрат {number} равен {result}");
+
+//Напиите программу, которая на вход принимает 2 числа и проверяет, 
+//является ли первое число квдратом второго.
+//a = 25, b = 5 -> да
+//a = 2, b = 10 -> нет
+//a = 9, b = -3 -> да
+//a = -3 b = 9 -> нет
+
+
+/*Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+        Console.WriteLine($"число {number1} является квадратом {number2}");
+}
+else 
+{
+    Console.WriteLine($"число {number1}  не является квадратом {number2}");
+}
+*/
+
+// Напишите программу, которая нп вход принимает одно число (N),
+//а на выходе показыввет все целые числа в промежутке от -N до N.
+
+/*
+Console.WriteLine("Дорогой пользователь, введи число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+//Напишите программу, которая принимает на вход трехзначное число и 
+//на выходе показывает последнюю цифру этого цисла.
+
+/*
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000){
+    int current = number % 10;
+    Console.WriteLine($"Последняя цифра числа {number} - {current}");
+
+}
+else
+{
+    Console.WriteLine("Число не трехзначное! ");
+}
+*/
+
